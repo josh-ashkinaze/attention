2f271cac10f247d416628773b97046e1f102e897
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-03-16 16:29:25 -0400

    commit attention scrape

---
+++ scripts/scrape_attention.py
@@ -0,0 +1,57 @@
+import logging
+import os
+import pandas as pd
+import time
+import random
+import subprocess
+import json
+
+
+def get_google_trends_data(kw, start_date, end_date, search_type, sleep_time=(1, 5)):
+    time.sleep(random.uniform(sleep_time[0], sleep_time[1]))
+    script_path = "get_google_trends.js"
+    result = subprocess.run(["node", script_path, kw, start_date, end_date, search_type], capture_output=True,
+                            text=True)
+    try:
+        if result.returncode == 0:
+            json_data = json.loads(result.stdout)
+            df = pd.DataFrame(json_data)
+            df['search_type'] = search_type
+            return df
+        else:
+            print("Error fetching data:", result.stderr)
+            return pd.DataFrame({'kw': kw, 'date': start_date, 'value': -1})
+    except Exception as e:
+        logging.info("Error fetching data for keyword {}: {}".format(kw,e))
+        return pd.DataFrame({'kw': kw, 'date': start_date, 'value': -1})
+
+def main():
+    log_file = os.path.splitext(os.path.basename(__file__))[0] + '.log'
+    logging.basicConfig(filename=log_file, level=logging.INFO, filemode='w', format='%(asctime)s %(message)s')
+    json_df = pd.read_json("../data/2023-03-16_15:44:46_rumors.json").T.reset_index()
+
+    # Convert the 'Rumor day' and 'Announcement day' columns to datetime objects
+    json_df['rumor_day'] = pd.to_datetime(json_df['rumor_day'])
+    json_df['announce_day'] = pd.to_datetime(json_df['announce_day'])
+    json_df['start_date'] = (json_df['rumor_day'] - pd.Timedelta(days=30)).dt.strftime('%Y-%m-%d')
+    json_df['end_date'] = (json_df['announce_day'] + pd.Timedelta(days=30)).dt.strftime('%Y-%m-%d')
+
+    # Calculate the days between 'Rumor day' and 'Announcement day'
+    json_df['days_between'] = (json_df['announce_day'] - json_df['rumor_day']).dt.days
+    logging.info("Number of events: {}".format(len(json_df)))
+    # Filter for gap between 7 and 60 days
+    json_df_filter = json_df.query("days_between>=7&days_between<=60")
+    logging.info("Number of events after filtering: {}".format(len(json_df_filter)))
+
+    all_data = []
+    for index, row in json_df_filter.iterrows():
+        kws = row['keywords']
+        search_type = ['web', 'search', 'youtube']
+        for kw in kws:
+            for search_type in search_type:
+                trend_data = get_google_trends_data(kw, row['start_date'], row['end_date'], search_type)
+                trend_data['event'] = row['index']
+                all_data.append(trend_data)
+
+    all_data_df = pd.merge(all_data)
+    all_data.to_csv("../data/trend_data.csv")
