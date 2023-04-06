da91afa9b85c7bd826bbf766c919530fd08b6487
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-04-06 17:12:27 -0400

    change to reddit comments

--- scripts/get_clean_data/scrape_attention.py
+++ scripts/get_clean_data/scrape_attention.py
@@ -9,7 +9,13 @@ import logging
 import os
 import pandas as pd
 import time
-import tenacity
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+    before_sleep_log,
+    retry_if_exception_type
+)
 import random
 import subprocess
 import numpy as np
@@ -25,7 +31,15 @@ from pmaw import PushshiftAPI
 from waybacknews.searchapi import SearchApiClient
 import datetime as dt
 
+class TooManyRequestsError(Exception):
+    pass
+
+def date_to_unix_timestamp(date_string):
+    dt = datetime.datetime.strptime(date_string, '%Y-%m-%d')
+    return int(time.mktime(dt.timetuple()))
+
 def get_news_data(kw, start_date, end_date):
+    """Get news counts from mediacloud"""
     start = dt.datetime.strptime(start_date, "%Y-%m-%d")
     end = dt.datetime.strptime(end_date, "%Y-%m-%d")
 
@@ -87,7 +101,11 @@ def get_wiki_data(kw, start_date, end_date):
     except Exception as e:
         logging.error(f"Error getting Wikipedia article for {kw}: {e}")
         return pd.DataFrame({'kw': kw, 'date': start_date, 'value': np.NaN, 'search_type': 'wikipedia'}), -1
-
+@retry(wait=wait_random_exponential(multiplier=1, min=1, max=60),
+       stop=stop_after_attempt(30),
+       retry=retry_if_exception_type(TooManyRequestsError),
+       before_sleep=before_sleep_log(logging, logging.INFO),
+       reraise=False)
 def get_reddit_data(kw, start_date, end_date):
     def date_to_unix_timestamp(date_string):
         dt = datetime.datetime.strptime(date_string, '%Y-%m-%d')
@@ -102,13 +120,19 @@ def get_reddit_data(kw, start_date, end_date):
             current_start = date_to_unix_timestamp(date.strftime('%Y-%m-%d'))
             current_end = date_to_unix_timestamp((date + pd.Timedelta(days=1)).strftime('%Y-%m-%d'))
             time.sleep(0.05)
-            url = f'https://api.pushshift.io/reddit/submission/search?title={kw}&since={current_start}&until={current_end}&limit=0&track_total_hits=true'
+            url = f'https://api.pushshift.io/reddit/comment/search?title={kw}&since={current_start}&until={current_end}&limit=0&track_total_hits=true'
             response = requests.get(url)
+            if response.status_code == 429:
+                raise TooManyRequestsError("Too many requests")
             data = response.json()
             total_hits = data['metadata']['es']['hits']['total']['value']
             data_list.append({'date': date, 'kw': kw, 'value': total_hits})
         df = pd.DataFrame(data_list)
         return df
+    except TooManyRequestsError as e:
+        logging.info(f"Too many requests for {kw}: {e}")
+        return pd.DataFrame(
+            {'kw': [str(kw)], 'date': [start_date], 'value': [np.NaN]}, index=[0])
     except Exception as e:
         logging.info(f"Error getting Reddit data for {kw}: {e}")
         return pd.DataFrame(
@@ -162,7 +186,7 @@ def get_google_trends_data(kw, start_date, end_date, search_type, sleep_multipli
             else:
                 print("Error fetching data:", result.stderr)
                 return pd.DataFrame(
-                    {'kw': [str(kw)], 'date': [start_date], 'value': [-1], 'search_type': [search_type]}, index=[0])
+                    {'kw': [str(kw)], 'date': [start_date], 'value': [np.NaN], 'search_type': [search_type]}, index=[0])
         except Exception as e:
             logging.info("Error fetching data (parsing response) for keyword {}: {}".format(kw, e))
             return pd.DataFrame({'kw': [str(kw)], 'date': [start_date], 'value': [np.NaN], 'search_type': [search_type]},
@@ -173,6 +197,8 @@ def get_google_trends_data(kw, start_date, end_date, search_type, sleep_multipli
 
 
 def main(debug=False, sleep_multiplier=1):
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
     log_file = os.path.splitext(os.path.basename(__file__))[0] + '.log'
     logging.basicConfig(filename=log_file, level=logging.INFO, filemode='w', format='%(asctime)s %(message)s')
     random.seed(416)
@@ -216,20 +242,16 @@ def main(debug=False, sleep_multiplier=1):
         # search_types = ['web', 'search', 'youtube', 'wiki', 'news', 'reddit']
         search_types = ['web', 'search', 'youtube', 'news', 'reddit']
 
-
         for kw in kws:
             for search_type in search_types:
                 logging.info("Processing keyword: {} for search type {}".format(kw, search_type))
                 if search_type == 'reddit':
                     trend_data = get_reddit_data(kw, row['start_date'], row['end_date'])
-                    trend_data['search_type'] = search_type
                 elif search_type == 'wiki':
                     trend_data, wiki_kw  = get_wiki_data(kw, row['start_date'], row['end_date'])
                     wiki_article_matches.append({'kw': kw, 'wiki_kw': wiki_kw})
-                    trend_data['search_type'] = search_type
                 elif search_type == 'news':
                     trend_data = get_news_data(kw, row['start_date'], row['end_date'])
-                    trend_data['search_type'] = search_type
                 else:
                     trend_data = get_google_trends_data(kw=kw,
                                                         start_date=row['start_date'],
@@ -237,6 +259,7 @@ def main(debug=False, sleep_multiplier=1):
                                                         search_type=search_type,
                                                         sleep_multiplier=sleep_multiplier)
                 trend_data['event'] = row['index']
+                trend_data['search_type'] = search_type
 
                 # Append the trend data to the CSV file
                 with open(fn, 'a', newline='') as f:
@@ -250,7 +273,7 @@ def main(debug=False, sleep_multiplier=1):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Scrape Google Trends data for keywords")
+    parser = argparse.ArgumentParser(description="Scrape data for keywords")
     parser.add_argument("--debug", action="store_true", help="Enable debug mode")
     parser.add_argument("--sleep", type=float, default=1, help="Sleep time multiplier")
     args = parser.parse_args()
